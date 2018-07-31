9ad5f2e5f7c8b2dfd0131cf2f156188355e5d6a5
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-31 18:36:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,3 +6,15 @@ end
 get '/' do
   "Hello, World!"
 end
+
+get '/name' do
+  "My name is Kyla"
+end
+
+get '/hometown' do
+  "My hometown is Seattle"
+end
+
+get '/favorite-song' do
+  "My favorite song is Saturn"
+end
